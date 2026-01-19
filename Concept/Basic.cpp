0b11569fc8e020d629e0915854624b79a4850a2b
DohunWi <widohun7@gmail.com>
2026-01-19 16:48:31 +0900

    First Commit: 환경 세팅 완료

---
+++ Concept/Basic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+// #include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+    // 입출력 속도 향상 (필수)
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "Hello C++ World!" << endl;
+
+    return 0;
+}
